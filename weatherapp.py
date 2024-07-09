1f54ee755bf36dcd6786c8b830ede36f426e7cf7
Shavidu Keshan <shavidu4321@gmail.com>
2024-07-09 13:20:09 +0530

    Add image for background

--- weatherapp.py
+++ weatherapp.py
@@ -16,8 +16,16 @@ class weatherClass:
         self.root.resizable(False, False)
         self.root.config(bg='#f8ea28', bd=1)
         
+        #background image#
+        self.image = Image.open("weather2.jpg")  # Update with your image path
+        self.resized_image = self.image.resize((700, 400))
+        self.photo_image = ImageTk.PhotoImage(self.resized_image)
+        self.image_label = Label(self.root, image=self.photo_image)
+        self.image_label.place(x=0, y=0)
+        
 if __name__ == "__main__":
     root = Tk()
     obj = weatherClass(root)
     root.mainloop()
+    
 
